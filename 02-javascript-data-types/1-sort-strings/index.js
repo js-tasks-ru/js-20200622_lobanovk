f4b7696c21e3e4bd684580fe3222241f1bb2fb60
lobanovk <lobanov.k@sarex.io>
2020-06-28 15:21:45 +0300

    Задача на сортировка массива

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,8 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
+export function sortStrings(arr, param = 'asc') {
+  const arrSort = [...arr].sort((word1, word2) => word1.localeCompare(word2, 'kf', {usage: 'sort', caseFirst : "upper"}));
 
+  return param === 'asc' ? arrSort : arrSort.reverse();
 }
