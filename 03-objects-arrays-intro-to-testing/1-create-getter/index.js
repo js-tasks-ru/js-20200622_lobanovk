44bf79649340c5b241fc600099ac6375c92aed44
lobanovk <lobanov.k@sarex.io>
2020-06-30 21:28:07 +0300

    Решена 1 задача

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const arr = path.split('.');
+  return (obj) => {
+    if (Object.keys(obj).length === 0) return undefined;
+    let result = {...obj}
+    arr.forEach(item => result = result[item] ? result[item] : undefined);
+    return result;
+  }
 }
