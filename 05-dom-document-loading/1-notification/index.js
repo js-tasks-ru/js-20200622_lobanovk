06faca91d7ef5e1f42e03e05703948051af5360c
lobanovk <lobanov.k@sarex.io>
2020-07-08 14:05:12 +0300

    1 задача. 5дз

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  defaultDuration = 20000;
+  element = null;
+  static visible = false;
 
+  constructor(message = '', {
+    duration = this.duration,
+    type = 'success',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.template;
+  }
+
+
+  get template() {
+    const time = this.duration / 1000;
+
+    const element = document.createElement('div');
+    element.setAttribute('class', `notification ${this.type}`);
+    element.setAttribute('style', `--value:${time}s`);
+
+    const content = `
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    `
+
+    element.innerHTML = content;
+
+    return element;
+  }
+
+  
+
+  show(node) {    
+    if (!NotificationMessage.visible) {
+      NotificationMessage.visible = true;
+      
+      node ? 
+        node.append(this.element) :
+        document.body.append(this.element);
+
+      setTimeout(() => this.remove(), this.duration)
+    }
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.visible = false;
+  }
+
+  destroy() {
+		this.remove();
+	}
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,35 @@
 export default class SortableTable {
+  element = null;
 
+  constructor(header = [], { data = [] } = {}) {
+    thos.header = header;
+    this.data = data;
+
+    this.render();
+  }
+
+  get templateHeader () {
+
+  }
+
+  get templateBodyTable() {
+
+  }
+
+  getRowBody () {
+    
+  }
+
+  render() {
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
 
